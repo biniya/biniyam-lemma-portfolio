8314536d8dc838659688b8707b571f5a5f07c9fa
biniya <lemmasbini@gmail.com>
2023-03-07 16:46:47 +0300

    Add inertia

---
+++ resources/js/Home.vue
@@ -0,0 +1,6 @@
+<template>
+    <h1 class="text-blue-700">
+        HOME
+    </h1>
+</template>
+<script setup></script>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('/')->group(function () {
+    Route::get('', function () {
+        return Inertia::render('Home');
+    });
 });
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
