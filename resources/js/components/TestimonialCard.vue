bd5d688df9e922ba24ad19651f574414a0ef1f17
Biniyam Lemma <lemmasbini@gmail.com>
2023-12-28 10:52:53 +0300

    Add `TestimonialCard` component

---
+++ resources/js/components/TestimonialCard.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps(["testimonial"]);
+</script>
+
+<template>
+    <div
+        class="flex w-full flex-col gap-2 bg-slate-200 p-6 transition-all duration-400 rounded-md cursor-pointer hover:scale-105 hover:opacity-100"
+    >
+        <div class="text-lg font-bold">{{ testimonial.title }}</div>
+        <p class="text-sm text-gray-600">
+            {{ testimonial.description }}
+        </p>
+        <div class="text-sm font-medium">- {{ testimonial.name }}</div>
+    </div>
+</template>
+
+<style scoped></style>
