d82824adbb23b84de53b40bfc330818ac89b7db6
Biniyam Lemma <lemmasbini@gmail.com>
2023-12-28 10:53:54 +0300

    Comment out `Contact` page and add `Testimonials` page in `Header`

--- resources/js/pages/Header.vue
+++ resources/js/pages/Header.vue
@@ -1,23 +1,25 @@
 <script setup>
-import Tab from "../Components/Tab.vue";
+import Tab from "../components/Tab.vue";
 import { ref } from "vue";
 import Projects from "./Projects.vue";
-import Contact from "./Contact.vue";
+// import Contact from "./Contact.vue";
 import Landing from "./Landing.vue";
 import About from "./About.vue";
+import Testimonials from "./Testimonials.vue";
 
 const tabs = [
     { title: "Home" },
     { title: "Projects" },
     { title: "About" },
-    { title: "Contact" },
+    { title: "Testimonials" },
+    // { title: "Contact" },
 ];
 
 const activeTab = ref("Home");
 </script>
 
 <template>
-    <div class="flex w-full items-center justify-center">
+    <div class="flex w-full z-50 items-center justify-center">
         <Tab v-model:active="activeTab" :tabs="tabs">
             <template #home>
                 <Landing />
@@ -28,9 +30,12 @@ const activeTab = ref("Home");
             <template #about>
                 <About />
             </template>
-            <template #contact>
-                <Contact />
+            <template #testimonials>
+                <Testimonials />
             </template>
+            <!--      <template #contact>-->
+            <!--        <Contact />-->
+            <!--      </template>-->
         </Tab>
     </div>
 </template>
