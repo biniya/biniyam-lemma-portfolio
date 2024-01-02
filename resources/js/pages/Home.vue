367631bdc104147ae37a7d382e66b9132a9fd993
Biniyam Lemma <lemmasbini@gmail.com>
2024-01-02 13:48:10 +0300

    Use the `BackgroundLayout` in `Home` page and fix UI there

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Header from "./Header.vue";
+import BackgroundLayout from "../layout/BackgroundLayout.vue";
+</script>
+<template>
+    <BackgroundLayout>
+        <div class="flex sm:w-full py-5 px-2 md:p-20 items-center md:h-full">
+            <Header />
+        </div>
+    </BackgroundLayout>
+</template>
